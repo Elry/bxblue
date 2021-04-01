80f4752f821fc2635baec47e45fb2ce425018e5e
Elry <rafelgarcialima@outlook.com>
2021-04-01 12:58:16 -0300

    better conn text

--- lib/db/conn.ts
+++ lib/db/conn.ts
@@ -5,5 +5,5 @@ const sql = sqlite3.verbose();
 export const db = new sql.Database('poke.db', sql.OPEN_READWRITE, (err:any):void => {
   if(err){ return console.log(err.message); }
   
-  console.log("Connected to in-memory storage");
+  console.log("Connected to db");
 });
