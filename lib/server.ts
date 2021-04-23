74bcd1301787170c898d5a22ee37756b648abd38
Elry <rafelgarcialima@outlook.com>
2021-04-23 16:12:10 -0300

    Swagger route added, trade route added, routes now using app only, better cors and compression config added, useless imports removed and controller logic moved to a proper controller.

--- lib/server.ts
+++ lib/server.ts
@@ -1,44 +1,21 @@
-import axios from "axios";
+import cors from "cors";
 import dotenv from "dotenv";
-import insert from "./db/insert";
-import select from "./db/select";
 import compression from "compression";
+import corsConfig from './utils/cors';
+import swaggerDoc from '../swagger.json';
+import swaggerUi from "swagger-ui-express";
+import tradeRoute from "./routes/trade.route";
 import express, {Request, Response, NextFunction} from "express";
 
 dotenv.config();
 
 const app = express();
-const router = express.Router();
-
-function enableCORS(req:Request, res:Response, next:NextFunction){
-  if (!process.env.DISABLE_XORIGIN) {
-    /* Test only*/
-    const allowedOrigins = ["*"]; 
-    const origin = req.headers.origin;
-    /* Test only*/
-
-    if (!process.env.XORIGIN_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
-      res.set({
-        "Access-Control-Allow-Origin": origin,
-        "Access-Control-Allow-Methods": "GET, POST",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-      });
-    }
-  }
-  next();
-};
-
-function shouldCompress(req:Request, res:Response){
-  if (req.headers['x-no-compression']) { return false; }
-  // standard filter
-  return compression.filter(req, res)
-}
 
 // reading received json data with default express
 app.use(express.json());
 
 // compression server responses
-app.use(compression({filter: shouldCompress}));
+app.use(compression());
 
 // get err
 app.use((err:any, req:Request, res:Response, next:NextFunction):void => {
@@ -49,114 +26,19 @@ app.use((err:any, req:Request, res:Response, next:NextFunction):void => {
   }
 });
 
-// 404
-app.use((req:Request, res:Response) => {
-  if (req.method.toLowerCase() === "options") {
-    res.end();
-  } else {
-    res.status(404).type("json").send("Not Found");
-  }
-});
-
 // setting basic cors
-app.use('/api', enableCORS, router);
+app.use(cors(corsConfig));
 
 // home
-app.get("/", (req:Request, res:Response):void => { 
+app.get('/', (req:Request, res:Response):void => { 
   res.status(200).json("PokeTrade: pokemon trade fairness calculator");
 });
 
+// routes
+app.use('/api/trade', tradeRoute);
+
+// swagger documentation
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 // initial listener
 app.listen(process.env.PORT, ():void => console.log('Server started successfully'));
-
-// GET /list
-app.get("/v1/trade/list", (req:Request, res:Response):void => {
-  try{
-    select((e:any):void => {
-      console.log(e);
-      res.status(200).json(e);
-    });  
-  }catch(err:any){
-    res.status(500).json(err);
-  }
-});
-
-// POST /check
-app.post("/v1/trade/check", async (req:Request, res:Response):Promise<void> => {
-  // variables to use as example in case none is given
-  const p1Ex:object = [
-    {"id": 1},
-    {"id": 2}
-  ];
-  
-  const p2Ex:object = [
-    {"id": 3},
-    {"id": 4}
-  ];
-
-  const user1:string = req.body.users.p1.name ? req.body.users.p1.name : "p1";
-  const user2:string = req.body.users.p2.name ? req.body.users.p2.name : "p2";
-  const p1:object[] = req.body.users.p1.pokemons ? req.body.users.p1.pokemons : p1Ex;  
-  const p2:object[] = req.body.users.p2.pokemons ? req.body.users.p2.pokemons : p2Ex;
-
-  try{
-    let p1Check:string = checkSize(p1, "p1");
-    let p2Check:string = checkSize(p2, "p2");  
-    
-    // checking size between 1 and 6
-    if(p1Check || p2Check){
-      res.status(400).json(p1Check + " " + p2Check);
-      return;
-    }
-  
-    let fair:string = await checkFairness(p1, p2);
-
-    if(fair){
-      insert([user1, user2], [p1, p2], fair);
-      res.status(200).json(fair);
-    }
-    else{ throw 0; }
-  }catch(err:any){
-    res.status(500).json(`Error: ${err}`);
-  }
-});
-
-function checkSize(obj:object[], name:string):string{
-  if(obj.length < 1){
-    return `${name} must provide at least one pokemon`;
-  }else if(obj.length > 6){
-    return `${name} must provide a maximum of 6 pokemons`
-  }
-  return "";
-}
-
-// get base experience
-async function getBaseExp(id:number):Promise<number>{
-  const resp = await axios.get(`https://${process.env.POKEURI}` + id);
-  return resp.data.base_experience;
-}
-
-async function sumValues(val:object):Promise<number>{
-  let sum:number = 0;  
-  
-  for(let i in val){
-    sum += await getBaseExp(val[i].id);
-  }
-
-  return sum;
-}
-
-async function checkFairness(p1:Object, p2:Object):Promise<string>{
-  let result:number = 0;
-  let p1Sum:number = await sumValues(p1);
-  let p2Sum:number = await sumValues(p2);
-
-  if(p1Sum > p2Sum){
-    result = p1Sum - p2Sum;
-  }else{
-    result = p2Sum - p1Sum;
-  }
-
-  let output:string = `p1 exp ${p1Sum} | p2 exp ${p2Sum}`
-  return (result <= 10) ? `Fair: ${output}` : `Not fair: ${output}`;
-}
